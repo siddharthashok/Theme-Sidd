fdff39b52648277dec8381fb05b31b1274958312
Siddharth Ashok <siddharthashokg@gmail.com>
2017-06-25 17:03:23 +0530

    rolling back tilt values it was buggy

--- js/app.js
+++ js/app.js
@@ -94,9 +94,9 @@ window.ondevicemotion = function(event) {
 	accX = Math.round(event.accelerationIncludingGravity.x) / 10;
 	accY = Math.round(event.accelerationIncludingGravity.y) / 10;
 
-	movement = 200;
+	movement = 100;
 
-	xA = -((accX * movement) + 120);
+	xA = -((accX * movement) + 100);
 	yA = -(accY) * movement;
 
 $('#mobile-header').css({'background-position' : xA+'px center'});
